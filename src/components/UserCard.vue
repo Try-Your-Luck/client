6fbcc612b99ec1872285ee9220e6ea5206061c74
hoandreas <andreas_ho@hotmail.com>
2019-12-12 19:05:47 +0700

    first commit

---
+++ src/components/UserCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col-lg-3">
+    <b-card :class='{"light" : (!this.isGreen), "success" : (this.isGreen)}' text-variant="black" header="USERNAME" class="text-center">
+      <b-button @click.prevent="ready" variant="primary">Ready</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  props: ['User'],
+  data () {
+    return {
+      isGreen: false
+    }
+  },
+  methods: {
+    ready () {
+      this.isGreen = true
+    }
+  }
+
+}
+</script>
+
+<style>
+.light {
+  background-color: white !important;
+}
+.success {
+  background-color: aquamarine !important;
+}
+</style>
---
+++ src/views/GameLobby.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="game-lobby">
+    <div class="row">
+      <UserCard></UserCard>
+      <UserCard></UserCard>
+      <UserCard></UserCard>
+      <UserCard></UserCard>
+    </div>
+    <div class="row mt-5">
+      <b-button size="lg" variant="warning">Start Game</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserCard from '../components/UserCard'
+export default {
+  name: 'GameLobby',
+  components: {
+    UserCard
+  }
+}
+</script>
+
+<style scoped>
+  .game-lobby {
+    background-image: url('https://images.squarespace-cdn.com/content/589135c903596e466b814f4c/1520907223472-YAKUGIB3ELBUS4MK90R9/poker_tournament_landscape_tv_full_hd.jpg?content-type=image%2Fjpeg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+
+  }
+  /* .row {
+    width: 100%;
+  } */
+</style>
