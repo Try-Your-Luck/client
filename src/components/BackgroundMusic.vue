5b5719b6e6f53018fce85335ae4a8dc1564723ad
hoandreas <andreas_ho@hotmail.com>
2019-12-13 10:13:47 +0700

    add background music

---
+++ src/components/BackgroundMusic.vue
@@ -0,0 +1,18 @@
+<template>
+  <audio autoplay>
+    <source src="../assets/username-lobby.mp3" type="audio/mpeg" />
+    <source src="../assets/room-list-lobby.mp3" type="audio/mpeg" />
+    <source src="../assets/waiting-lobby.mp3" type="audio/mpeg" />
+    <source src="../assets/main-game-room.mp3" type="audio/mpeg" />
+  </audio>
+</template>
+
+<script>
+export default {
+  name: 'BackgroundMusic'
+}
+</script>
+
+<style>
+
+</style>
